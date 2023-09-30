047991e97e75cb00b7178c475ba835792cf75fd5
lolieatapple <lolieatapple@gmail.com>
2023-10-01 00:05:41 +0800

    add mobile UI

--- app/page.js
+++ app/page.js
@@ -9,22 +9,22 @@ export default function Home() {
         <img src="/banner.png" alt="banner" className="w-full" />
 
         {/* Big title */}
-        <h1 className="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:text-5xl font-bold text-[#000]">
+        <h1 className="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:text-2xl lg:text-4xl xl:text-5xl font-bold text-[#000]">
           Crypto Donations
         </h1>
 
         {/* Subtitle */}
-        <p className="absolute mt-5 top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:text-xl text-[#000] hidden md:block">
+        <p className="absolute mt-10 top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:text-lg text-[#000] hidden md:block">
           Seamless and secure way for users to donate native coins or ERC20 tokens.
         </p>
 
         {/* Button and its note */}
         <div className="absolute mt-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center">
-          <button className="bg-[#76c6cd] hover:bg-[#5ba9b5] text-white py-2 px-5 rounded-lg shadow-xl transition-colors duration-300 mb-1">
+          <button className="xl:mt-5 bg-[#76c6cd] hover:bg-[#5ba9b5] text-white py-2 px-5 rounded-lg shadow-xl transition-colors duration-300 mb-1">
             Connect Wallet
           </button>
-          <p className="mt-2 text-sm text-gray-500 hidden md:block">
-            * Connect wallet to create yourself donation links.
+          <p className="xl:mt-2 text-sm text-gray-500 hidden md:block">
+            * Connect wallet to view & create yourself donation links.
           </p>
         </div>
       </div>
